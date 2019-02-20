3e33a19a910a3bba3cc834af633c05ff7fcc4fc9
Isaiah Nields <inields@inields.local>
2019-02-19 20:30:58 -0500

    Add unit norm visualization

--- visualization/unit_norms.py
+++ visualization/unit_norms.py
@@ -10,14 +10,18 @@ plt.axis('equal')
 # get the subplots for plotting
 fig, ax = plt.subplots()
 
+ax.set_aspect('equal', adjustable='box')
+
+ax.set_ylim([-1.2, 1.2])
+ax.set_xlim([-1.2, 1.2])
+
 # plot the data
-c = plt.plot(alpha=0.75)
+line = ax.plot([0], [0], alpha=0.75)
 
 
 def update(p):
-    # Update the line and the axes (with a new xlabel). Return a tuple of
-    # "artists" that have to be redrawn for this frame.
 
+    print('{:1.3f}'.format(p))
     # initialize values for when y is positive
     x1 = np.linspace(-1, 1, 500)
     x2 = (1 - (np.abs(x1) ** p)) ** (1 / p)
@@ -26,14 +30,18 @@ def update(p):
     x1 = np.concatenate((x1, np.flip(x1)), axis=0).reshape(1, 1000)
     x2 = np.concatenate((x2, -x2), axis=0).reshape(1, 1000)
 
-    c.set_xdata(x1)
-    c.set_ydata(x2)
-    plt.title('Matrix norm: p = ' + str(p))
+    for l, a, b in zip(line, x1, x2):
+        l.set_xdata(x1)
+        l.set_ydata(x2)
 
-    return c, ax
+    plt.title('All vectors with norm = 1 and p = {:1.3f}'.format(p))
+
+    return line, ax
+
 
 # create the animation
-anim = FuncAnimation(fig, update, frames=np.linspace(0.1, 200, 100), interval=50)
+# ps = np.concatenate((np.linspace(0.0, 1.0, 50), np.linspace(1.01, 5.0, 50), np.linspace(5.01, 50.0, 50)), axis=0)
+anim = FuncAnimation(fig, update, frames=np.logspace(-1.0, 2.0, 30), interval=1)
 anim.save('unit_norms.gif', dpi=150, writer='imagemagick')
 
 plt.show()
